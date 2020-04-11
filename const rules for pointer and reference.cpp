326598d4627e6560e0b223af23818a8ca0029d1f
r-AZAD-jr <55370357+r-AZAD-jr@users.noreply.github.com>
2020-04-11 13:49:59 +0200

    Update const rules for pointer and reference.cpp

--- const rules for pointer and reference.cpp
+++ const rules for pointer and reference.cpp
@@ -5,15 +5,15 @@ int main()
 
 	int const* pNumber = &Number;
 	pNumber = &Number_1;
-	//	*pNumber = 10;
+	// *pNumber = 10;
 	
 	int* const pNumber_1 = &Number;
 	*pNumber_1 = 20;
-	//	pNumber_1 = & Number_1;
+	// pNumber_1 = & Number_1;
 
 	int const* const pNumber_3 = &Number;
-	//	*pNumber_3 = & Number_1;
-	//	pNumber_3 = &Number_1;
+	// *pNumber_3 = & Number_1;
+	// pNumber_3 = &Number_1;
 	int Number_2 = *pNumber_3;
 	int const* pNumber_4 = pNumber_3;
 
